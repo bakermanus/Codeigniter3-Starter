c211001f2a180b99f20f7a4a990aa47a5dba7e34
Lucas B <lucasborio@gmail.com>
2017-12-03 14:29:22 -0300

    login auth sytem

--- application/libraries/Bcrypt.php
+++ application/libraries/Bcrypt.php
@@ -1,19 +1,48 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+/**
+ * Name:    Bcrypt
+ *
+ * Requirements: PHP5 or above
+ *
+ * @package    CodeIgniter-Ion-Auth
+ * @author     Ben Edmunds
+ * @link       http://github.com/benedmunds/CodeIgniter-Ion-Auth
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-
-class Bcrypt {
+/**
+ * Class Bcrypt
+ */
+class Bcrypt
+{
+  /**
+   * @var int
+   */
   private $rounds;
+
+  /**
+   * @var string
+   */
   private $salt_prefix;
+
+  /**
+   * @var int|string|null
+   */
+  private $randomState;
 
   /**
    * Bcrypt constructor.
    *
    * @param array $params
+   *
    * @throws Exception
    */
-  public function __construct($params=array('rounds'=>7, 'salt_prefix'=>'$2y$')) {
+  public function __construct($params = array('rounds' => 7, 'salt_prefix' => '$2y$'))
+  {
 
-    if(CRYPT_BLOWFISH != 1) {
+    if (CRYPT_BLOWFISH != 1)
+    {
       throw new Exception("bcrypt not supported in this installation. See http://php.net/crypt");
     }
 
@@ -21,28 +50,84 @@ class Bcrypt {
     $this->salt_prefix = $params['salt_prefix'];
   }
 
-  public function hash($input) {
+  /**
+   * @param string $input
+   *
+   * @return bool|string
+   */
+  public function hash($input)
+  {
     $hash = crypt($input, $this->getSalt());
 
-    if(strlen($hash) > 13) {
+    if (strlen($hash) > 13)
+    {
       return $hash;
     }
 
-    return false;
+    return FALSE;
   }
 
   /**
-   * @param $input
-   * @param $existingHash
+   * @param string $input
+   * @param string $existingHash
+   *
    * @return bool
-     */
-  public function verify($input, $existingHash) {
+   */
+  public function verify($input, $existingHash)
+  {
     $hash = crypt($input, $existingHash);
-    return $hash === $existingHash;
+    return $this->hashEquals($existingHash, $hash);
   }
 
-  private function getSalt() {
-    $salt = sprintf($this->salt_prefix.'%02d$', $this->rounds);
+  /**
+   * Polyfill for hash_equals()
+   * Code mainly taken from hash_equals() compat function of CodeIgniter 3
+   *
+   * @param  string $known_string
+   * @param  string $user_string
+   *
+   * @return  bool
+   */
+  private function hashEquals($known_string, $user_string)
+  {
+    // For CI3 or PHP >= 5.6
+    if (function_exists('hash_equals'))
+    {
+      return hash_equals($known_string, $user_string);
+    }
+
+    // For CI2 with PHP < 5.6
+    // Code from CI3 https://github.com/bcit-ci/CodeIgniter/blob/develop/system/core/compat/hash.php
+    if (!is_string($known_string))
+    {
+      trigger_error('hash_equals(): Expected known_string to be a string, ' . strtolower(gettype($known_string)) . ' given', E_USER_WARNING);
+      return FALSE;
+    }
+    else if (!is_string($user_string))
+    {
+      trigger_error('hash_equals(): Expected user_string to be a string, ' . strtolower(gettype($user_string)) . ' given', E_USER_WARNING);
+      return FALSE;
+    }
+    else if (($length = strlen($known_string)) !== strlen($user_string))
+    {
+      return FALSE;
+    }
+
+    $diff = 0;
+    for ($i = 0; $i < $length; $i++)
+    {
+      $diff |= ord($known_string[$i]) ^ ord($user_string[$i]);
+    }
+
+    return ($diff === 0);
+  }
+
+  /**
+   * @return string
+   */
+  private function getSalt()
+  {
+    $salt = sprintf($this->salt_prefix . '%02d$', $this->rounds);
 
     $bytes = $this->getRandomBytes(16);
 
@@ -51,43 +136,52 @@ class Bcrypt {
     return $salt;
   }
 
-  private $randomState;
-
-
   /**
    * @param $count
+   *
    * @return string
-     */
-  private function getRandomBytes($count) {
+   */
+  private function getRandomBytes($count)
+  {
     $bytes = '';
 
-    if(function_exists('openssl_random_pseudo_bytes') &&
-        (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN')) { // OpenSSL slow on Win
+    if (function_exists('openssl_random_pseudo_bytes') &&
+      (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN'))
+    {
+      // OpenSSL slow on Win
       $bytes = openssl_random_pseudo_bytes($count);
     }
 
-    if($bytes === '' && @is_readable('/dev/urandom') &&
-       ($hRand = @fopen('/dev/urandom', 'rb')) !== FALSE) {
+    if ($bytes === '' && @is_readable('/dev/urandom') &&
+      ($hRand = @fopen('/dev/urandom', 'rb')) !== FALSE)
+    {
       $bytes = fread($hRand, $count);
       fclose($hRand);
     }
 
-    if(strlen($bytes) < $count) {
+    if (strlen($bytes) < $count)
+    {
       $bytes = '';
 
-      if($this->randomState === null) {
+      if ($this->randomState === NULL)
+      {
         $this->randomState = microtime();
-        if(function_exists('getmypid')) {
+        if (function_exists('getmypid'))
+        {
           $this->randomState .= getmypid();
         }
       }
 
-      for($i = 0; $i < $count; $i += 16) {
+      for ($i = 0; $i < $count; $i += 16)
+      {
         $this->randomState = md5(microtime() . $this->randomState);
 
-        if (PHP_VERSION >= '5') {
-          $bytes .= md5($this->randomState, true);
-        } else {
+        if (PHP_VERSION >= '5')
+        {
+          $bytes .= md5($this->randomState, TRUE);
+        }
+        else
+        {
           $bytes .= pack('H*', md5($this->randomState));
         }
       }
@@ -99,20 +193,24 @@ class Bcrypt {
   }
 
   /**
-   * @param $input
+   * @param string $input
+   *
    * @return string
-     */
-  private function encodeBytes($input) {
+   */
+  private function encodeBytes($input)
+  {
     // The following is code from the PHP Password Hashing Framework
     $itoa64 = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
     $output = '';
     $i = 0;
-    do {
+    do
+    {
       $c1 = ord($input[$i++]);
       $output .= $itoa64[$c1 >> 2];
       $c1 = ($c1 & 0x03) << 4;
-      if ($i >= 16) {
+      if ($i >= 16)
+      {
         $output .= $itoa64[$c1];
         break;
       }
@@ -131,6 +229,3 @@ class Bcrypt {
     return $output;
   }
 }
-
-
-/***** End of BCrypt.php ***********/
--- ci3-starter.sql
+++ ci3-starter.sql
@@ -1,48 +1,39 @@
+DROP TABLE IF EXISTS `groups`;
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `groups` */
+#
+# Table structure for table 'groups'
+#
 
 CREATE TABLE `groups` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `description` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-/*Data for the table `groups` */
-
-insert  into `groups`(`id`,`name`,`description`) values 
-(1,'admin','Administrator'),
-(2,'members','General User');
-
-/*Table structure for table `login_attempts` */
-
-CREATE TABLE `login_attempts` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `ip_address` varchar(15) NOT NULL,
-  `login` varchar(100) NOT NULL,
-  `time` int(11) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `login_attempts` */
+#
+# Dumping data for table 'groups'
+#
 
-/*Table structure for table `users` */
+INSERT INTO `groups` (`id`, `name`, `description`) VALUES
+     (1,'admin','Administrator'),
+     (2,'members','General User');
+
+
+
+DROP TABLE IF EXISTS `users`;
+
+#
+# Table structure for table 'users'
+#
 
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `ip_address` varchar(15) NOT NULL,
-  `username` varchar(100) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `username` varchar(100) NULL,
   `password` varchar(255) NOT NULL,
   `salt` varchar(255) DEFAULT NULL,
-  `email` varchar(100) NOT NULL,
+  `email` varchar(254) NOT NULL,
   `activation_code` varchar(40) DEFAULT NULL,
   `forgotten_password_code` varchar(40) DEFAULT NULL,
   `forgotten_password_time` int(11) unsigned DEFAULT NULL,
@@ -55,34 +46,51 @@ CREATE TABLE `users` (
   `company` varchar(100) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `users` */
 
-insert  into `users`(`id`,`ip_address`,`username`,`password`,`salt`,`email`,`activation_code`,`forgotten_password_code`,`forgotten_password_time`,`remember_code`,`created_on`,`last_login`,`active`,`first_name`,`last_name`,`company`,`phone`) values 
-(1,'127.0.0.1','administrator','$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36','','admin@admin.com','',NULL,NULL,NULL,1268889823,1494528365,1,'Admin','istrator','ADMIN','0');
+#
+# Dumping data for table 'users'
+#
 
-/*Table structure for table `users_groups` */
+INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
+     ('1','127.0.0.1','administrator','$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36','','admin@admin.com','',NULL,'1268889823','1268889823','1', 'Admin','istrator','ADMIN','0');
+
+
+DROP TABLE IF EXISTS `users_groups`;
+
+#
+# Table structure for table 'users_groups'
+#
 
 CREATE TABLE `users_groups` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL,
   `group_id` mediumint(8) unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
   KEY `fk_users_groups_users1_idx` (`user_id`),
   KEY `fk_users_groups_groups1_idx` (`group_id`),
-  CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  CONSTRAINT `uc_users_groups` UNIQUE (`user_id`, `group_id`),
+  CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `users_groups` */
-
-insert  into `users_groups`(`id`,`user_id`,`group_id`) values 
-(1,1,1),
-(2,1,2);
+INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
+     (1,1,1),
+     (2,1,2);
 
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+DROP TABLE IF EXISTS `login_attempts`;
+
+#
+# Table structure for table 'login_attempts'
+#
+
+CREATE TABLE `login_attempts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(45) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `time` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
